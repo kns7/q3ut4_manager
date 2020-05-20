41326d56af5e417a193cc4475c46413b13bcf9e0
Nicolas Kapfer <nico@kns7.org>
2020-05-20 18:13:43 +0200

    Added RCON Controller and Python Script

---
+++ app/Controller/RCONController.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Controller\Controller;
+
+class RCONController extends Controller
+{
+
+    protected $command;
+    public function setMap($map)
+    {
+        
+    }
+
+    public function setGametype($type)
+    {
+
+    }
+
+    public function getMap()
+    {
+
+    }
+
+    public function getGametype()
+    {
+
+    }
+
+    public function getPlayers()
+    {
+        
+    }
+
+}
